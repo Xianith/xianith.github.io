ee54a618f73d3bc01df1904a77005934860f7dcf
Xianith <xianith@gmail.com>
2016-09-20 16:07:57 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" href="css/main.css" type="text/css" media="all">
+	<script src="java/main.js"></script>
+	<title>Xianith</title>
+
+	<noscript>
+		<style type="text/css">
+			#tabs {
+				display: none;
+			}
+
+			#container h2 {
+				display: block;
+			}
+		</style>
+	</noscript>
+
+	</head>
+
+	<body onload="init()">
+
+<div id="header">
+		<h1>Xianith</h1>
+	</div>
+
+	<ul id="tabs">
+		<li><a href="#about" onclick="showTab()">Me</a></li>
+		<li><a href="#work" onclick="showTab()">Work</a></li>
+		<li><a href="#contact" onclick="showTab()">Contact</a></li>
+	</ul>
+
+	<div id="container">
+		<section class="tabContent" id="about">
+		<h2>Welcome</h2>
+
+		<hr>
+
+		Hello world...
+		
+		</section>
+
+		<section class="tabContent" id="work">
+		<h2>Welcome</h2>
+
+		<hr>
+
+		Workworkworkworkwork
+		
+		</section>
+
+		<section class="tabContent" id="contact">
+		<h2>Welcome</h2>
+
+		<hr>
+
+		My telephone...
+		
+		</section>
+
+	</div>
+
+	</body>
+</html>
